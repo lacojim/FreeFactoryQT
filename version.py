c273019b0201c09c3660a24c0b233d0e27f91f10
James Hines <lacojim@gmail.com>
2025-09-20 02:31:05 -0400

    Add files via upload
    Added Constant Frame Rate (CFR) option to gui and ffmpeg cmd builder.

---
+++ version.py
@@ -0,0 +1,11 @@
+"""
+version.py – Central version definition for FreeFactoryQT.
+"""
+
+__version__ = "1.1.32-dev"
+
+def get_version() -> str:
+    """
+    Returns the current FreeFactoryQT version string.
+    """
+    return __version__
