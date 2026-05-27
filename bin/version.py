667c3f70523b9a7e5ffb5629b30573f3e8adecba
James Hines <lacojim@gmail.com>
2026-05-26 23:42:05 -0400

    Add files via upload

--- bin/version.py
+++ bin/version.py
@@ -1,7 +1,7 @@
 # version.py – Central version definition for FreeFactoryQT.
 
 
-__version__ = "1.1.50-dev"
+__version__ = "1.1.51-dev"
 
 def get_version() -> str:
     # Returns the current FreeFactoryQT version string.
